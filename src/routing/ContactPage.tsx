cc1ab17aa72f0c6c7fc92ba0593c585765c9608e
fahimekhandan <fahimekhandan@gmail.com>
2024-06-21 15:41:29 +0330

    feat: routing with params

--- src/routing/ContactPage.tsx
+++ src/routing/ContactPage.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const ContactPage = () => {
+  const navigation = useNavigate();
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        // Redirect the user to the home page
+        navigation("/");
       }}
     >
       <button className="btn btn-primary">Submit</button>
--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,12 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  // const params = useParams();
+  // const [searchparams, setSearchparams] = useSearchParams();
+  // console.log(searchparams.get("name"));
+
+  // const location= useLocation()
+
   return <p>User</p>;
 };
 
